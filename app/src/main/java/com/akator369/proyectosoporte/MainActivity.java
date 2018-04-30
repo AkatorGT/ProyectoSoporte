4302a9a0f0bcf3228028cef8e743743162906159
AkatorGT <james.akator@gmail.com>
2018-04-29 22:22:35 -0500

    1c

---
+++ app/src/main/java/com/akator369/proyectosoporte/MainActivity.java
@@ -0,0 +1,13 @@
+package com.akator369.proyectosoporte;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
